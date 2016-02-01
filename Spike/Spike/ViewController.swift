400bcb3ee62a8300ef052d3d625b2e830c4d9d65
Jose Torres <jose.torres@santexgroup.com>
2016-02-01 10:35:47 -0500

    Setting base project structure.

---
+++ Spike/Spike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spike
+//
+//  Created by Jose Torres Cardenas on 1/02/16.
+//  Copyright © 2016 santex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
